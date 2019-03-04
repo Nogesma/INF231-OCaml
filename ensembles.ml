79740db81bd03c3cd47dd419936b61d5baf31f98
Benjamin Phénix <Ben@eduroam-105102.grenet.fr>
2019-03-04 09:27:27 +0100

    Ajout de la fonction SupFromSet qui supprime un element donné.

--- ensembles.ml
+++ ensembles.ml
@@ -9,12 +9,17 @@ Mano Ségransan
 (* Q1: *)
 
 type 'a ensemble =                  (* ’a est le réservoir d’éléments *)
-        | Ve                        (* V mis pour «vide», e pour «ensemble» *)
-        | Ce of 'a * 'a ensemble;;  (* C mis pour « constructeur » *)
+        | Es                        (* V mis pour «vide», e pour «ensemble» *)
+        | Cs of 'a * 'a ensemble;;  (* C mis pour « constructeur » *)
 
 
-let rec cardinal = function Ve -> 0 | Ce (_, l) -> cardinal l + 1;;
+let rec cardinal = function Es -> 0 | Cs (_, l) -> cardinal l + 1;;
 
-let rec isInList e = function Ve -> false | Ce (x, lp) -> e = x || (e > x) && isInList e lp;;
+let rec isInList e = function Es -> false | Cs (x, lp) -> e = x || (e > x) && isInList e lp;;
 
-let rec isIncludedIn l = function Ve -> true | Ce (x, lp) -> isInList x l && isIncludedIn l lp;;
+let rec isIncludedIn l = function Es -> true | Cs (x, lp) -> isInList x l && isIncludedIn l lp;;
+
+let ajoute (a : _) (e : 'a ensemble) = if isInList a e then e else Cs(a,e);;
+
+let rec supFromSet e = function Es -> Es | Cs (x, lp) -> lp ;;
+
