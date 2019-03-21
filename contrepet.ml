ec676195638cda61ad61ccc25bf32d8d24d3ba23
Nogesma <mano.segransan@gmail.com>
2019-03-21 13:16:13 +0100

    Add more contrepet functions

--- contrepet.ml
+++ contrepet.ml
@@ -20,12 +20,46 @@ type 'a set =
 
 type dictionnary = word set;;
 
-let add dic word =  Cs((List.init (String.length word) (String.get word)), dic);;
+let wordToList w = (List.init (String.length w) (String.get w));;
+let add dic word =  Cs((wordToList word), dic);;
 
 let cst_DICO = Es;;
-
 let cst_DICO = add cst_DICO "quelle";;
 let cst_DICO = add cst_DICO "ministre";;
 let cst_DICO = add cst_DICO "seche";;
 let cst_DICO = add cst_DICO "sinistre";;
 let cst_DICO = add cst_DICO "meche";;
+let cst_DICO = add cst_DICO "il";;
+let cst_DICO = add cst_DICO "fait";;
+let cst_DICO = add cst_DICO "chaud";;
+let cst_DICO = add cst_DICO "et";;
+let cst_DICO = add cst_DICO "beau";;
+
+type phrase = word list;;
+
+let supprimePrefixeCommun w1 w2 = 
+  if (List.hd w1) = (List.hd w2) 
+  then ((List.tl w1), (List.tl w2)) 
+  else (w1,w2)
+;;
+
+let suffixeEgaux w1 w2 = 
+  (List.tl w1) = (List.tl w2)
+;;
+
+let motSontContrepet w1 w2 = suffixeEgaux w1 w2;;
+
+assert (wordToList "test" = ['t'; 'e'; 's'; 't']);;
+assert (wordToList "" = []);;
+
+assert (add Es "test" = Cs(['t'; 'e'; 's'; 't'], Es));;
+assert (add (Cs(['t'; 'e'; 's'; 't'], Es)) "mot" = Cs(['m'; 'o'; 't'], Cs(['t'; 'e'; 's'; 't'], Es)));;
+
+assert (supprimePrefixeCommun (wordToList "test") (wordToList "ton") = (['e'; 's'; 't'], ['o'; 'n']));;
+assert (supprimePrefixeCommun (wordToList "test") (wordToList "on") = (['t'; 'e'; 's'; 't'], ['o'; 'n']));;
+
+assert (suffixeEgaux (wordToList "test") (wordToList "on") = false);;
+assert (suffixeEgaux (wordToList "test") (wordToList "cest") = true);;
+
+assert (motSontContrepet (wordToList "test") (wordToList "cest") = true);;
+assert (motSontContrepet (wordToList "test") (wordToList "ton") = false);;
