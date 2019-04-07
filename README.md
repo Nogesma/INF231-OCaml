e3d15c4408a7ba4d857d218d8f7041d0c80ece4e
Nogesma <mano.segransan@gmail.com>
2019-04-07 20:20:20 +0200

    Update functions name, readme, and some comments

--- README.md
+++ README.md
@@ -1,26 +1,27 @@
-
-#  Ocaml Project - INF231 - 2019
+# Ocaml Project - INF231 - 2019
 
 By **Benjamin Phénix** & **Mano Ségransan**
-
 
 ## Project Structure
 
 The projet is constitued of one file per part:
 
-
 ### 2) Ensembles
 
-The code is situated in the `sets.ml` file.
+The code is in the `sets.ml` file.
 
-### 3) Réusinage: listes Ocamlet ordre supérieur
+### 3) Réusinage: listes Ocaml et ordre supérieur
 
-The code is situated in the `refactor.ml` file.
+The code is in the `refactor.ml` file.
 
 ### 4) Multi-ensembles
 
-The code is situated in the `multiSets.ml` file.
+The code is in the `multiSets.ml` file.
 
 ### 5) Contrepet
 
-The code is situated in the `contrepet.ml` file.
+The code is in the `contrepet.ml` file.
+
+### 6) Optimisations et extensions
+
+The code for Q17 is in the `setOpti.ml` file.
--- contrepet.ml
+++ contrepet.ml
@@ -25,21 +25,21 @@ type dictionnary = word set;;
 (* Q8: *)
 
 let wordToList (w :string) :char list = (List.init (String.length w) (String.get w));;
-let add (dic :dictionnary) (word :string) :dictionnary =  Cs((wordToList word), dic);;
+let addWordToDic (dic :dictionnary) (word :string) :dictionnary =  Cs((wordToList word), dic);;
 
 (* Q9: *)
 
 let cst_DICO = Es;;
-let cst_DICO = add cst_DICO "quelle";;
-let cst_DICO = add cst_DICO "ministre";;
-let cst_DICO = add cst_DICO "seche";;
-let cst_DICO = add cst_DICO "sinistre";;
-let cst_DICO = add cst_DICO "meche";;
-let cst_DICO = add cst_DICO "il";;
-let cst_DICO = add cst_DICO "fait";;
-let cst_DICO = add cst_DICO "chaud";;
-let cst_DICO = add cst_DICO "et";;
-let cst_DICO = add cst_DICO "beau";;
+let cst_DICO = addWordToDic cst_DICO "quelle";;
+let cst_DICO = addWordToDic cst_DICO "ministre";;
+let cst_DICO = addWordToDic cst_DICO "seche";;
+let cst_DICO = addWordToDic cst_DICO "sinistre";;
+let cst_DICO = addWordToDic cst_DICO "meche";;
+let cst_DICO = addWordToDic cst_DICO "il";;
+let cst_DICO = addWordToDic cst_DICO "fait";;
+let cst_DICO = addWordToDic cst_DICO "chaud";;
+let cst_DICO = addWordToDic cst_DICO "et";;
+let cst_DICO = addWordToDic cst_DICO "beau";;
 
 (* Q10: *)
 
@@ -47,43 +47,44 @@ type phrase = word list;;
 
 (* Q11: *)
 
-let supprimePrefixeCommun (w1 :word) (w2 :word) :word * word =
+let deletePrefix (w1 :word) (w2 :word) :word * word =
   if (List.hd w1) = (List.hd w2)
   then ((List.tl w1),(List.tl w2))
   else (w1, w2)
 ;;
 
-let suffixeEgaux (w1 :word) (w2 :word) :bool =
+let sameSuffix (w1 :word) (w2 :word) :bool =
   w1 <> w2 && (List.tl w1) = (List.tl w2)
 ;;
 
 (* Q12: *)
 
-let motsSontContrepets ((m1, m1') :word * word) ((m2, m2') :word * word) :bool =
-  suffixeEgaux m1 m2
-  && suffixeEgaux m1' m2'
-  && supprimePrefixeCommun m1 m2' = supprimePrefixeCommun m2 m1'
+let wordsAreSpoonerisms ((m1, m1') :word * word) ((m2, m2') :word * word) :bool =
+  sameSuffix m1 m2
+  && sameSuffix m1' m2'
+  && deletePrefix m1 m2' = deletePrefix m2 m1'
 ;;
 
 (* Q13: *)
 
-let phraseContrepet (p1 :phrase) (p2 :phrase) :bool =
+let phraseAreSpoonerisms (p1 :phrase) (p2 :phrase) :bool =
   let x = (List.filter (fun e -> List.for_all (fun x -> x <> e) p2) p1) in
   let y = (List.filter (fun e -> List.for_all (fun x -> x <> e) p1) p2) in
   List.length x = 2
   && List.length y = 2
-  && motsSontContrepets (List.nth x 0, List.nth x 1) (List.nth y 0, List.nth y 1)
+  && wordsAreSpoonerisms (List.nth x 0, List.nth x 1) (List.nth y 0, List.nth y 1)
  ;;
 
 (* Q14: *)
 
-(* range is a subfunction wich is responsible for decomposing a certain part of a word *)
+(* range is a subfunction used by sublist, which creates an array of numbers ranging from i to j *)
 let rec range ?i:(i=0) (j :int) :int list =
   if i >= j
   then []
   else i :: (range ~i:(i + 1) j)
 ;;
 
+(* sublist is a subfunction used by splitWord, which return the part of the given list within the given indexes *)
 let sublist (l :word) (s: int) (e :int) :word =
   List.map (fun (_, x) -> x)
     (List.filter (fun (i, _) -> i >= s && i < e)
@@ -91,10 +92,10 @@ let sublist (l :word) (s: int) (e :int) :word =
 ;;
 
 (* The recursive function wich deals with the word and separation and put the sublist in another list *)
-let rec decompose ?i:(i=0) (w :word) :'a list =
+let rec splitWord ?i:(i=0) (w :word) :'a list =
   if i = (List.length w)
   then []
-  else (sublist w 0 i, (List.nth w i), sublist w (i + 1) (List.length w))::(decompose ~i:(i + 1) w)
+  else (sublist w 0 i, (List.nth w i), sublist w (i + 1) (List.length w))::(splitWord ~i:(i + 1) w)
 ;;
 
 (* Q15: *)
@@ -122,21 +123,21 @@ let swap
 assert (wordToList "test" = ['t'; 'e'; 's'; 't']);;
 assert (wordToList "" = []);;
 
-assert (add Es "test" = Cs(['t'; 'e'; 's'; 't'], Es));;
-assert (add (Cs(['t'; 'e'; 's'; 't'], Es)) "mot" = Cs(['m'; 'o'; 't'], Cs(['t'; 'e'; 's'; 't'], Es)));;
+assert (addWordToDic Es "test" = Cs(['t'; 'e'; 's'; 't'], Es));;
+assert (addWordToDic (Cs(['t'; 'e'; 's'; 't'], Es)) "mot" = Cs(['m'; 'o'; 't'], Cs(['t'; 'e'; 's'; 't'], Es)));;
 
-assert (supprimePrefixeCommun (wordToList "test") (wordToList "ton") = (['e'; 's'; 't'], ['o'; 'n']));;
-assert (supprimePrefixeCommun (wordToList "test") (wordToList "on") = (['t'; 'e'; 's'; 't'], ['o'; 'n']));;
+assert (deletePrefix (wordToList "test") (wordToList "ton") = (['e'; 's'; 't'], ['o'; 'n']));;
+assert (deletePrefix (wordToList "test") (wordToList "on") = (['t'; 'e'; 's'; 't'], ['o'; 'n']));;
 
-assert (suffixeEgaux (wordToList "test") (wordToList "on") = false);;
-assert (suffixeEgaux (wordToList "test") (wordToList "cest") = true);;
+assert (sameSuffix (wordToList "test") (wordToList "on") = false);;
+assert (sameSuffix (wordToList "test") (wordToList "cest") = true);;
 
-assert (motsSontContrepets ((wordToList "ministre"),(wordToList "seche")) ((wordToList "sinistre"),(wordToList "meche")) = true);;
-assert (motsSontContrepets ((wordToList "sinistre"),(wordToList "seche")) ((wordToList "sinistre"),(wordToList "meche")) = false);;
-assert (motsSontContrepets ((wordToList "linistre"),(wordToList "seche")) ((wordToList "sinistre"),(wordToList "meche")) = false);;
+assert (wordsAreSpoonerisms ((wordToList "ministre"),(wordToList "seche")) ((wordToList "sinistre"),(wordToList "meche")) = true);;
+assert (wordsAreSpoonerisms ((wordToList "sinistre"),(wordToList "seche")) ((wordToList "sinistre"),(wordToList "meche")) = false);;
+assert (wordsAreSpoonerisms ((wordToList "linistre"),(wordToList "seche")) ((wordToList "sinistre"),(wordToList "meche")) = false);;
 
-assert (phraseContrepet [(wordToList "quelle"); (wordToList "min"); (wordToList "seche")] [(wordToList "quelle"); (wordToList "sin"); (wordToList "meche")] = true);;
-assert (phraseContrepet [(wordToList "quelle"); (wordToList "sin"); (wordToList "seche")] [(wordToList "quelle"); (wordToList "min"); (wordToList "meche")] = false);;
+assert (phraseAreSpoonerisms [(wordToList "quelle"); (wordToList "min"); (wordToList "seche")] [(wordToList "quelle"); (wordToList "sin"); (wordToList "meche")] = true);;
+assert (phraseAreSpoonerisms [(wordToList "quelle"); (wordToList "sin"); (wordToList "seche")] [(wordToList "quelle"); (wordToList "min"); (wordToList "meche")] = false);;
 
 assert (range 5 = [0; 1; 2; 3; 4]);;
 assert (range ~i:3 5 = [3; 4]);;
@@ -146,11 +147,11 @@ assert (sublist (wordToList "test") 0 0 = []);;
 assert (sublist (wordToList "test") 0 3 = ['t'; 'e'; 's']);;
 assert (sublist (wordToList "test") 2 3 = ['s']);;
 
-assert (decompose (wordToList "test") = [([], 't', ['e'; 's'; 't']); (['t'], 'e', ['s'; 't']); (['t'; 'e'], 's', ['t']); (['t'; 'e'; 's'], 't', [])]);;
-assert (decompose (wordToList "sin") = [([], 's', ['i'; 'n']); (['s'], 'i', ['n']); (['s'; 'i'], 'n', [])]);;
+assert (splitWord (wordToList "test") = [([], 't', ['e'; 's'; 't']); (['t'], 'e', ['s'; 't']); (['t'; 'e'], 's', ['t']); (['t'; 'e'; 's'], 't', [])]);;
+assert (splitWord (wordToList "sin") = [([], 's', ['i'; 'n']); (['s'], 'i', ['n']); (['s'; 'i'], 'n', [])]);;
 
-assert (swap (List.nth (decompose (wordToList "test")) 1) (List.nth (decompose (wordToList "sint")) 1) = ((['t'], 'i', ['s'; 't']), (['s'], 'e', ['n'; 't'])));;
-assert (swap (List.nth (decompose (wordToList "sin")) 0) (List.nth (decompose (wordToList "min")) 0) = (([], 'm', ['i'; 'n']), ([], 's', ['i'; 'n'])));;
+assert (swap (List.nth (splitWord (wordToList "test")) 1) (List.nth (splitWord (wordToList "sint")) 1) = ((['t'], 'i', ['s'; 't']), (['s'], 'e', ['n'; 't'])));;
+assert (swap (List.nth (splitWord (wordToList "sin")) 0) (List.nth (splitWord (wordToList "min")) 0) = (([], 'm', ['i'; 'n']), ([], 's', ['i'; 'n'])));;
 
 
 
